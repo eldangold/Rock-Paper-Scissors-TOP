3c90765da10364b3ec9fa6985e448025a1d2490f
Eldan Gold <56773148+eldangold@users.noreply.github.com>
2024-07-29 20:44:51 +0200

    Step 5 (playRound function)

--- script.js
+++ script.js
@@ -20,3 +20,26 @@ function getHumanChoice() {
     let humanChoice = prompt("Type your choice:");
     return humanChoice;
 }
+
+function playRound(humanChoice, computerChoice) {
+    if (computerChoice === humanChoice) {
+        console.log("Tie");
+    }
+
+    else if (
+        (humanChoice === "paper" && computerChoice === "rock") ||
+        (humanChoice === "scissors" && computerChoice === "paper") ||
+        (humanChoice === "rock" && computerChoice === "scissors")
+     ) {
+        console.log("You win!",humanChoice,"beats",computerChoice);  
+    }
+
+    else {
+        console.log("You lose!",computerChoice,"beats",humanChoice);
+    }
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
