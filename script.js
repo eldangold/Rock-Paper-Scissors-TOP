0a50780e8eb2e97bc9d8035959a256ff9874fe5a
Eldan Gold <56773148+eldangold@users.noreply.github.com>
2024-07-29 17:41:59 +0200

    Add switch statements for step 2

--- script.js
+++ script.js
@@ -2,7 +2,18 @@ let humanScore = 0;
 let computerScore = 0;
 
 function getComputerChoice() {
-    let computerChoice = Math.floor(Math.random() *3);
+    let computerChoice = Math.floor(Math.random() * 3 + 1);
+    switch (computerChoice) {
+    case 1:
+        return "rock";
+        break;
+    case 2:
+        return "scissors";
+        break;
+    case 3:
+        return "paper";
+        break;        
+    }
 }
 
 function getHumanChoice() {
